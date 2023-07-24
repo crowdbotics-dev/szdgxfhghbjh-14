2806538cfc246f896b3359a9c6c3937fe8a6d396
Crowdbotics <engineering@crowdbotics.com>
2023-07-24 16:36:13 +0000

    Update store.

--- store/szdgxfhghbjhAPI/api.js
+++ store/szdgxfhghbjhAPI/api.js
@@ -1,76 +1,80 @@
-import axios from "axios";
+import axios from "axios"
 const szdgxfhghbjhAPI = axios.create({
   baseURL: "https://szdgxfhghbjh-14.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return szdgxfhghbjhAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return szdgxfhghbjhAPI.post(`/api/v1/login/`, payload);
+  return szdgxfhghbjhAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return szdgxfhghbjhAPI.post(`/api/v1/signup/`, payload);
+  return szdgxfhghbjhAPI.post(`/api/v1/signup/`, payload)
 }
-
+function modules_files_uploads_list(payload) {
+  return szdgxfhghbjhAPI.get(`/modules/files/uploads/`)
+}
+function modules_files_uploads_create(payload) {
+  return szdgxfhghbjhAPI.post(`/modules/files/uploads/`, payload)
+}
+function modules_files_uploads_retrieve(payload) {
+  return szdgxfhghbjhAPI.get(`/modules/files/uploads/${payload.id}/`)
+}
+function modules_files_uploads_update(payload) {
+  return szdgxfhghbjhAPI.put(`/modules/files/uploads/${payload.id}/`, payload)
+}
+function modules_files_uploads_partial_update(payload) {
+  return szdgxfhghbjhAPI.patch(`/modules/files/uploads/${payload.id}/`, payload)
+}
+function modules_files_uploads_destroy(payload) {
+  return szdgxfhghbjhAPI.delete(`/modules/files/uploads/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
-  return szdgxfhghbjhAPI.post(`/rest-auth/login/`, payload);
+  return szdgxfhghbjhAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return szdgxfhghbjhAPI.get(`/rest-auth/logout/`);
+  return szdgxfhghbjhAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return szdgxfhghbjhAPI.post(`/rest-auth/logout/`);
+  return szdgxfhghbjhAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return szdgxfhghbjhAPI.post(`/rest-auth/password/change/`, payload);
+  return szdgxfhghbjhAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return szdgxfhghbjhAPI.post(`/rest-auth/password/reset/`, payload);
+  return szdgxfhghbjhAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return szdgxfhghbjhAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return szdgxfhghbjhAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return szdgxfhghbjhAPI.post(`/rest-auth/registration/`, payload);
+  return szdgxfhghbjhAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return szdgxfhghbjhAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return szdgxfhghbjhAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return szdgxfhghbjhAPI.get(`/rest-auth/user/`);
+  return szdgxfhghbjhAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return szdgxfhghbjhAPI.put(`/rest-auth/user/`, payload);
+  return szdgxfhghbjhAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return szdgxfhghbjhAPI.patch(`/rest-auth/user/`, payload);
+  return szdgxfhghbjhAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_files_uploads_list,
+  modules_files_uploads_create,
+  modules_files_uploads_retrieve,
+  modules_files_uploads_update,
+  modules_files_uploads_partial_update,
+  modules_files_uploads_destroy,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
@@ -82,4 +86,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
---
+++ store/szdgxfhghbjhAPI/fileUploads.slice.js
@@ -0,0 +1,174 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const modules_files_uploads_list = createAsyncThunk(
+  "fileUploads/modules_files_uploads_list",
+  async payload => {
+    const response = await apiService.modules_files_uploads_list(payload)
+    return response.data
+  }
+)
+export const modules_files_uploads_create = createAsyncThunk(
+  "fileUploads/modules_files_uploads_create",
+  async payload => {
+    const response = await apiService.modules_files_uploads_create(payload)
+    return response.data
+  }
+)
+export const modules_files_uploads_retrieve = createAsyncThunk(
+  "fileUploads/modules_files_uploads_retrieve",
+  async payload => {
+    const response = await apiService.modules_files_uploads_retrieve(payload)
+    return response.data
+  }
+)
+export const modules_files_uploads_update = createAsyncThunk(
+  "fileUploads/modules_files_uploads_update",
+  async payload => {
+    const response = await apiService.modules_files_uploads_update(payload)
+    return response.data
+  }
+)
+export const modules_files_uploads_partial_update = createAsyncThunk(
+  "fileUploads/modules_files_uploads_partial_update",
+  async payload => {
+    const response = await apiService.modules_files_uploads_partial_update(
+      payload
+    )
+    return response.data
+  }
+)
+export const modules_files_uploads_destroy = createAsyncThunk(
+  "fileUploads/modules_files_uploads_destroy",
+  async payload => {
+    const response = await apiService.modules_files_uploads_destroy(payload)
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const fileUploadsSlice = createSlice({
+  name: "fileUploads",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [modules_files_uploads_list.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [modules_files_uploads_list.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = action.payload
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_list.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_create.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [modules_files_uploads_create.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities.push(action.payload)
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_create.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_retrieve.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [modules_files_uploads_retrieve.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = [
+          ...state.entities.filter(record => record.id !== action.payload.id),
+          action.payload
+        ]
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_retrieve.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_update.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [modules_files_uploads_update.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record =>
+          record.id === action.payload.id ? action.payload : record
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_update.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_partial_update.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [modules_files_uploads_partial_update.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.map(record =>
+          record.id === action.payload.id ? action.payload : record
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_partial_update.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_destroy.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [modules_files_uploads_destroy.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = state.entities.filter(
+          record => record.id !== action.meta.arg?.id
+        )
+        state.api.loading = "idle"
+      }
+    },
+    [modules_files_uploads_destroy.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    }
+  }
+})
+export default {
+  modules_files_uploads_list,
+  modules_files_uploads_create,
+  modules_files_uploads_retrieve,
+  modules_files_uploads_update,
+  modules_files_uploads_partial_update,
+  modules_files_uploads_destroy,
+  slice: fileUploadsSlice
+}
