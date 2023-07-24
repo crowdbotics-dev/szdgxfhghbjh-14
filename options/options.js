63fe5dae6463803d0d05b98fa1714e9ded756aa3
Hesbon Kiptoo <hesbon.kiptoo@crowdbotics.com>
2023-07-24 19:57:46 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "szdgxfhghbjh-14",
-  url: "https://szdgxfhghbjh-14.botics.co",
-  api: "https://szdgxfhghbjh-14.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "szdgxfhghbjh-14",
+  "url": "https://szdgxfhghbjh-14.botics.co",
+  "api": "https://szdgxfhghbjh-14.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
